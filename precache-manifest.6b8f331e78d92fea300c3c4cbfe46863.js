f2d289b91f19595d2591c982ec2c7276832369d8
Delzad <delzadbamji@gmail.com>
2021-05-24 17:46:10 -0400

    Updates

---
+++ precache-manifest.6b8f331e78d92fea300c3c4cbfe46863.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c6acd97c68691b5b6411fe17f2ec9",
+    "url": "/Fetch-GitHub-Users/index.html"
+  },
+  {
+    "revision": "a28299b1aeef3435c22f",
+    "url": "/Fetch-GitHub-Users/static/css/2.ac18de59.chunk.css"
+  },
+  {
+    "revision": "f39bbdfe2a6a7b38b0f4",
+    "url": "/Fetch-GitHub-Users/static/css/main.64a54056.chunk.css"
+  },
+  {
+    "revision": "a28299b1aeef3435c22f",
+    "url": "/Fetch-GitHub-Users/static/js/2.e1d86dab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Fetch-GitHub-Users/static/js/2.e1d86dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f39bbdfe2a6a7b38b0f4",
+    "url": "/Fetch-GitHub-Users/static/js/main.221c7488.chunk.js"
+  },
+  {
+    "revision": "51ac66cc45be7d75ff41",
+    "url": "/Fetch-GitHub-Users/static/js/runtime-main.bbfe296f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Fetch-GitHub-Users/static/media/SEGA.12a3db8b.woff"
+  }
+]);
